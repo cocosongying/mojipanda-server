fb77949a1a2af1961a3f99ecf91d50642e5af0fd
cocosongying <cocosongying@gmail.com>
2020-05-16 12:09:09 +0800

    refactor:移除helloworld

--- router/index.js
+++ router/index.js
@@ -1,16 +1,11 @@
 const Checker = require('../common/checker');
 const Filter = require('../common/filter');
-const HelloWorld = require('../controller/helloworld');
 const User = require('../controller/user');
 const { Role } = require('../const/userattr');
 
 const filters = [
     ["/user/login", Filter.noCheck],
     ["/", Filter.checkToken],
-];
-
-const hello = [
-    ["/hello", HelloWorld.content],
 ];
 
 const user = [
@@ -23,6 +18,5 @@ const user = [
 
 module.exports = [
     Checker(filters),
-    [hello, "/hello"],
     [user, "/user"],
 ];
