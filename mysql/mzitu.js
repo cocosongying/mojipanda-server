97e98b9345bd24513c873fbc519de7d118b066ea
cocosongying <cocosongying@gmail.com>
2020-09-01 21:52:03 +0800

    refactor:拆分两张表

--- mysql/mzitu.js
+++ mysql/mzitu.js
@@ -1,26 +1,38 @@
 const { client } = require('./connection');
 const { getFields } = require('../common/util/strs');
-const fields = ['id', 'parentId', 'url', 'title', 'type', 'createTime'];
+const gridFields = ['id', 'parentId', 'url', 'createTime'];
+const fields = ['id', 'url', 'title', 'type', 'createTime'];
 
 async function add(info) {
-    let { parentId, url, title, type, createTime } = info
-    let args = [parentId, url, title, type, createTime];
-    let sql = `insert into mzitu(parentId, url, title, type, createTime) 
-        values (?,?,?,?,?)`;
+    let { url, title, type, createTime } = info
+    let args = [url, title, type, createTime];
+    let sql = `insert into mzitu(url, title, type, createTime) 
+        values (?,?,?,?)`;
     let res = await client.query(sql, args);
     return res[0];
 }
 
+async function addGrid(info) {
+    let { parentId, url, createTime } = info
+    let args = [parentId, url, createTime];
+    let sql = `insert into mzitu_grid(parentId, url, createTime) 
+        values (?,?,?)`;
+    let res = await client.query(sql, args);
+    return res[0];
+}
+
+// 获取详细数据
 async function findByParentId(parentId, cols) {
-    let sql = `select ?? from mzitu where parentId = ? and isDeleted = 0`;
-    let args = [getFields(cols, fields), parentId];
+    let sql = `select ?? from mzitu_grid where parentId = ? and isDeleted = 0`;
+    let args = [getFields(cols, gridFields), parentId];
     let res = await client.query(sql, args);
     return res[0];
 }
 
+// 获取列表数据
 async function fingByType(opts, cols) {
     let { type, start, limit } = opts;
-    let sql = `select ?? from mzitu where parentId = 0 and isDeleted = 0 `;
+    let sql = `select ?? from mzitu where isDeleted = 0 `;
     let args = [getFields(cols, fields)];
     if (type) {
         sql += `and type = ? `;
@@ -34,6 +46,7 @@ async function fingByType(opts, cols) {
 
 module.exports = {
     add,
+    addGrid,
     findByParentId,
     fingByType,
 }
